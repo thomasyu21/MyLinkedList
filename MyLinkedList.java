ad395a5b98013db2a8a4d730213b93153b76e60f
thomasyu21 <thomasyu085@gmail.com>
2021-01-07 17:04:08 -0500

    Created a constructor as well as the size() method.

---
+++ MyLinkedList.java
@@ -0,0 +1,13 @@
+public class MyLinkedList{
+ private int size;
+ private Node start,end;
+ public MyLinkedList(){
+   size = 0;
+   start = null;
+   end = null;
+ }
+
+ public int size(){
+   return size;
+ }
+}
